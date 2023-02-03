7215ea2abe91e57a84c54f36e6a871ff15205a06
avcimehmet <trainitforfuture@gmail.com>
2023-02-04 00:42:13 +0100

    plan output and JSON format added for visualization

--- green_intsnace.tf
+++ green_intsnace.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "Blue-green-deployment-green" {
   ami                    = data.aws_ami.amzlinux.id
   instance_type          = var.instance_type
   subnet_id              = element(local.subnets, count.index)
-  vpc_security_group_ids = [aws_security_group.blue-green-deployment.id]
+  vpc_security_group_ids = [aws_security_group.Blue-green-deployment.id]
 
    user_data = templatefile("./apache-script.sh", {
     file_content = "green version 1.1 - ${count.index}"
